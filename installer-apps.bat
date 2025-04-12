6a194cbb06650b3bd208b35c20abdaef655dd768
JesusHelloJesusYoWassup <osusowa@gmail.com>
2025-04-12 22:48:54 +0300

    Update installer-apps.bat

--- installer-apps.bat
+++ installer-apps.bat
@@ -1,17 +1,32 @@
 @echo off
-powershell -NoProfile -ExecutionPolicy Bypass -Command ^
-"$apps = @(
-    @{Name='Discord';ID='Discord.Discord'},
-    @{Name='Spotify';ID='Spotify.Spotify'},
-    @{Name='Spicetify';ID='spicetify'},
-    @{Name='Telegram';ID='Telegram.TelegramDesktop'},
-    @{Name='Opera GX';ID='Opera.OperaGX'},
-    @{Name='7-Zip';ID='7zip.7zip'},
-    @{Name='Notepad++';ID='Notepad++.Notepad++'},
-    @{Name='Steam';ID='Valve.Steam'},
-    @{Name='Lightshot';ID='Skillbrains.Lightshot'}
-); foreach ($app in $apps) {
-    Write-Host \"Устанавливаю $($app.Name)...\";
-    winget install --id $($app.ID) -e --silent
-}; Write-Host '✅ Готово!'"
+echo Устанавливаю приложения...
+
+winget install --id Discord.Discord -e --silent
+echo Установлен Discord.
+
+winget install --id Spotify.Spotify -e --silent
+echo Установлен Spotify.
+
+winget install --id spicetify -e --silent
+echo Установлен Spicetify.
+
+winget install --id Telegram.TelegramDesktop -e --silent
+echo Установлен Telegram.
+
+winget install --id Opera.OperaGX -e --silent
+echo Установлен Opera GX.
+
+winget install --id 7zip.7zip -e --silent
+echo Установлен 7-Zip.
+
+winget install --id Notepad++.Notepad++ -e --silent
+echo Установлен Notepad++.
+
+winget install --id Valve.Steam -e --silent
+echo Установлен Steam.
+
+winget install --id Skillbrains.Lightshot -e --silent
+echo Установлен Lightshot.
+
+echo ✅ Готово!
 pause
